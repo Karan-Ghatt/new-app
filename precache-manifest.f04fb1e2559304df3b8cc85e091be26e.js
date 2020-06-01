217b3b8a5844eb9546ea90b127ecaa18207a2396
Karan-Ghatt <karanvir.ghatt@googlemail.com>
2020-06-01 02:03:11 +0100

    Updates

---
+++ precache-manifest.f04fb1e2559304df3b8cc85e091be26e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf844084c80ea59a9664803e8df148d",
+    "url": "/Karan-Ghatt/new-app/index.html"
+  },
+  {
+    "revision": "e132720e31f4033838e8",
+    "url": "/Karan-Ghatt/new-app/static/css/main.f2084247.chunk.css"
+  },
+  {
+    "revision": "ae4e3fa89f350a97ec16",
+    "url": "/Karan-Ghatt/new-app/static/js/2.6d8871a7.chunk.js"
+  },
+  {
+    "revision": "e132720e31f4033838e8",
+    "url": "/Karan-Ghatt/new-app/static/js/main.bfe10d19.chunk.js"
+  },
+  {
+    "revision": "67b6d26e8a23b58c435f",
+    "url": "/Karan-Ghatt/new-app/static/js/runtime-main.99c14301.js"
+  }
+]);
